43ad076aeab4305d4cf7a8785ec9dc8500649367
mithulcrafts <codinggraphicsdesiging@gmail.com>
2026-02-06 21:10:19 +0530

    After making errorHandler #20

---
+++ backend/src/middleware/errorHandler.js
@@ -0,0 +1,39 @@
+const constants=require('../constants/constants');
+errorHandler=(err,req,res,next)=>{
+    const statusCode=res.statusCode ? res.statusCode : 500;
+    switch(statusCode){
+        case constants.VALIDATION_ERROR:
+            res.json({"Title":"Validation Error",
+                "message":err.message,
+                "stackTrack":err.stack
+            });
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({"Title":"UNAUTHORIZED",
+                "message":err.message,
+                "stackTrack":err.stack
+            });
+            break;
+        case constants.FORBIDDEN:
+            res.json({"Title":"FORBIDDEN",
+                "message":err.message,
+                "stackTrack":err.stack
+            });
+            break;
+        case constants.NOT_FOUND:
+            res.json({"Title":"NOT FOUND",
+                "message":err.message,
+                "stackTrack":err.stack
+            });
+            break;
+        case constants.SERVER_ERROR:
+            res.json({"Title":"Server Error",
+                "message":err.message,
+                "stackTrack":err.stack
+            });
+            break;
+        default:
+            console.log("No Error, All good !!");
+            break;
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 const express=require('express');
+const errorHandler=require('./middleware/errorHandler');
 const app=express();
 require('dotenv').config();
 const PORT=process.env.PORT||3000;
-
+app.use(errorHandler());
 app.listen(PORT,()=>{
     console.log(`Server is running on the port ${PORT}`);
 })
