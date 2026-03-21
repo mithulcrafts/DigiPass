ed92633e42cef8adf7952f2ad3e97a8161fe5ade
mithulcrafts <codinggraphicsdesiging@gmail.com>
2026-03-21 23:29:25 +0530

    After making header closes #33 and correcting the student dashboard layout

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import "./styles/Header.css";
+import Logo from "../assets/Images/LogoRemovedbg.png";
+import Title from "../assets/Images/Title.png";
+import { CgProfile } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
+export default function Header() {
+  const navigate=useNavigate();
+  function redirect(){
+      navigate("/Student/Dashboard");
+  }
+  return (
+    <>
+      <div className="header">
+        <div className="logo" onClick={redirect}>
+          <img src={Logo} alt="Logo" className="symbol" />
+          <img src={Title} alt="Title" className="title" />
+        </div>
+        <CgProfile className="profileIcon"/>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/Student/StudentDashboard.jsx
+++ frontend/src/pages/Student/StudentDashboard.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { StudentPassHistory } from "../../components/Cards/PassHistory";
+import Header from "../../components/Header";
 import getUser from "../../utils/getUser";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
 import getOutpasses from "../../utils/getOutpasses";
+import './styles/StudentDashboard.css';
 export default function StudentDashboard() {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
@@ -19,30 +21,33 @@ export default function StudentDashboard() {
   }, []);
   return (
     <>
-      <h1>Hello {user ? user.name : "bro"} !!</h1>
-      {outpasses.length > 0 ? (
-        outpasses.map((outpass) => {
-          return (
-            <StudentPassHistory
-              key={outpass._id}
-              id={outpass._id}
-              Purpose={outpass.purpose}
-              Status={outpass.status}
-              FromDate={new Date(outpass.fromTime).toLocaleDateString()}
-              ToDate={new Date(outpass.toTime).toLocaleDateString()}
-              Destination={outpass.location}
-            />
-          );
-        })
-      ) : (
-        <p>No outpasses requested</p>
-      )}
-      <Button
+      <Header />
+      <h1 className="welcomeMessage">Hello {user ? user.name : "bro"} !!</h1>
+      <div className="passContainer">
+        {outpasses.length > 0 ? (
+          outpasses.map((outpass) => {
+            return (
+              <StudentPassHistory
+                key={outpass._id}
+                id={outpass._id}
+                Purpose={outpass.purpose}
+                Status={outpass.status}
+                FromDate={new Date(outpass.fromTime).toLocaleDateString()}
+                ToDate={new Date(outpass.toTime).toLocaleDateString()}
+                Destination={outpass.location}
+              />
+            );
+          })
+        ) : (
+          <p>No outpasses requested</p>
+        )}
+        <Button
         content="Request outpass"
         onClick={() => {
           navigate("/student/Outpass");
         }}
       />
+      </div>
     </>
   );
 }
